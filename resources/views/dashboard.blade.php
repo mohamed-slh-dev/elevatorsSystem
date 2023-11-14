dc2406ff03189b91a4fa10f6d415c3652ff5592c
aymoon.23@outlook.com <aymoon.23@outlook.com>
2023-11-14 18:41:05 +0300

    Heading Editing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,6 @@
     
 <div class="col-12">
     
- <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa porro sit aut ratione et? Fugiat, culpa amet?</h1>
+ <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa porro sit aut ratione et? Fugiat, culpa amet? Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
 </div>
 @endsection
