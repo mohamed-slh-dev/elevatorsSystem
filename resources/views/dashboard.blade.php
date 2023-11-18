16f0a9c135a5024ba684de1434d63584f0263401
aymoon.23@outlook.com <aymoon.23@outlook.com>
2023-11-18 23:50:59 +0300

    Dash.2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,13 @@
 
 {{-- :altering the align flow --}}
 <style>
+
+    .overview--title {
+        font-size: 17px;
+        letter-spacing: 0.8px
+    }
+
+
     .page-wrapper .page-body-wrapper .page-header .row {
         align-items: start;
     }
@@ -52,46 +59,158 @@
 <div class="col-12"></div>
 
 
+{{-- left column --}}
+<div class="col-12">
+    <div class="container-fluid general-widget">
+        <div class="row">
+
+
+            {{-- item --}}
+            <div class="col-sm-3 col-xl-3 col-lg-3">
+                <div class="card o-hidden border-0">
+                    <div class="bg-primary b-r-4 card-body">
+                        <div class="media static-top-widget">
+                            <div class="align-self-center text-center">
+                                <i data-feather="users"></i>
+                            </div>
+                            <div class="media-body">
+                                <span class="m-0 fw-bold  overview--title">العملاء</span>
+                                <h4 class="mb-0 counter fs-3">15</h4>
+                                <i class="icon-bg" data-feather="users"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- end item --}}
 
 
 
 
-<div class="col-xl-7 box-col-7">
+
+
+
+            {{-- item --}}
+            <div class="col-sm-3 col-xl-3 col-lg-3">
+                <div class="card o-hidden border-0">
+                    <div class="bg-secondary b-r-4 card-body">
+                        <div class="media static-top-widget">
+                            <div class="align-self-center text-center">
+                                <i data-feather="codepen"></i>
+                            </div>
+                            <div class="media-body">
+                                <span class="m-0 fw-bold  overview--title">المصاعد</span>
+                                <h4 class="mb-0 counter fs-3">32</h4>
+                                <i class="icon-bg" data-feather="codepen"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- end item --}}
+
+
+
+
+
+
+
+
+            {{-- item --}}
+            <div class="col-sm-3 col-xl-3 col-lg-3">
+                <div class="card o-hidden border-0">
+                    <div class="bg-primary b-r-4 card-body">
+                        <div class="media static-top-widget">
+                            <div class="align-self-center text-center">
+                                <i data-feather="settings"></i>
+                            </div>
+                            <div class="media-body">
+                                <span class="m-0 fw-bold  overview--title">أعمال التركيب</span>
+                                <h4 class="mb-0 counter fs-3">85</h4>
+                                <i class="icon-bg" data-feather="settings"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- end item --}}
+
+
+
+
+
+            {{-- item --}}
+            <div class="col-sm-3 col-xl-3 col-lg-3">
+                <div class="card o-hidden border-0">
+                    <div class="bg-secondary b-r-4 card-body">
+                        <div class="media static-top-widget">
+                            <div class="align-self-center text-center">
+                                <i data-feather="shield"></i>
+                            </div>
+                            <div class="media-body">
+                                <span class="m-0 fw-bold  overview--title">أعمال الصيانة</span>
+                                <h4 class="mb-0 counter fs-3">50</h4>
+                                <i class="icon-bg" data-feather="shield"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- end item --}}
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+{{-- right column --}}
+<div class="col-xl-7 box-col-7 d-none">
     <div class="widget-joins card" style="border: 2px solid #e9f0ee; border-radius: 5px;">
         <div class="row">
-        <div class="col-sm-6 pe-0">
-            <div class="media border-after-xs">
-            <div class="align-self-center me-3 fw-bold fs-5">
-                68%<i class="fa fa-angle-up ms-2"></i>
+
+            {{-- item --}}
+            <div class="col-sm-6 pe-0">
+                <div class="media border-after-xs">
+                <div class="align-self-center me-3 fw-bold fs-5">
+                    22%<i class="fa fa-angle-up ms-2"></i>
+                </div>
+                <div class="media-body details ps-3 text-center">
+                    <span class="mb-1">أعمال التركيب</span>
+                    <h5 class="mb-0 counter">{{$installationBills->count()}}</h5>
+                </div>
+                <div class="media-body align-self-center">
+                    <i
+                    class="font-primary float-end ms-2"
+                    data-feather="settings"></i>
+                </div>
+                </div>
             </div>
-            <div class="media-body details ps-3 text-center">
-                <span class="mb-1">أعمال التركيب</span>
-                <h5 class="mb-0 counter">35</h5>
+
+
+
+            {{-- item --}}
+            <div class="col-sm-6 ps-0">
+                <div class="media">
+                <div class="align-self-center me-3 fw-bold fs-5">
+                    12%<i class="fa fa-angle-down ms-2"></i>
+                </div>
+                <div class="media-body details ps-3 text-center">
+                    <span class="mb-1">أعمال الصيانة</span>
+                    <h5 class="mb-0 counter">{{$installationQuotations->count()}}</h5>
+                </div>
+                <div class="media-body align-self-center">
+                    <i
+                    class="font-primary float-end ms-3"
+                    data-feather="shield"></i>
+                </div>
+                </div>
             </div>
-            <div class="media-body align-self-center">
-                <i
-                class="font-primary float-end ms-2"
-                data-feather="settings"></i>
-            </div>
-            </div>
-        </div>
-        <div class="col-sm-6 ps-0">
-            <div class="media">
-            <div class="align-self-center me-3 fw-bold fs-5">
-                12%<i class="fa fa-angle-down ms-2"></i>
-            </div>
-            <div class="media-body details ps-3 text-center">
-                <span class="mb-1">أعمال الصيانة</span>
-                <h5 class="mb-0 counter">14</h5>
-            </div>
-            <div class="media-body align-self-center">
-                <i
-                class="font-primary float-end ms-3"
-                data-feather="shield"></i>
-            </div>
-            </div>
-        </div>
-        
+
         </div>
     </div>
 </div>
